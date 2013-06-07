7272f99fc60f2dc19fed8bf725dee128b67bcb4b
Olga Nikolova <olga.nikolova@sagebase.org>
2013-06-07 16:35:04 -0600

    Update mapChPos2hg19.R
    map split into maps per chromosome (linear query complexity ~O(chromosome length)
    added 2 columns - length of donor and acceptor gene (note modified format in synapse)

--- mapChPos2hg19.R
+++ mapChPos2hg19.R
@@ -11,40 +11,39 @@ library(biomaRt)
 ensembl=useMart("ensembl",dataset="hsapiens_gene_ensembl")
 map <- getBM(c("ensembl_gene_id","hgnc_symbol","chromosome_name","start_position","end_position"), mart = ensembl)
 
-clist <- as.list(c("BLCA","BRCA","CESC","HNSC","KICH","KIRC","KIRP","LGG","LIHC","LUAD",
-                  "LUSC","PAAD","PRAD","SKCM","THCA"))
-
-path <- "/Shares/work/DAT_109__TCGA_unc_rnaseq_genefusion/Data/.mapped2TCGA_final/"
-d.clist <- lapply(clist, function(type){
-  name <- paste(path, type,".txt", sep="")
-  return(read.csv(name, header=F, sep="\t"))
-})
+# List of submaps per chrom
+mapchr <- lapply(as.list(c(1:22,"X","Y")),function(x){return(map[map$chromosome_name == x,])})
+names(mapchr) <- c(1:22,"X","Y")
 
 # Retreive ensembl_id and hugo_symbol for 
 # given chromosome and position (positive, relative)
-# Output: string formatted:
-#     <E1>,<E2>,...,<En>;<H1>,<H2>,...,<Hn>
+# Output: vector of 2 strings formatted:
+#     "<E1>,<E2>,...,<En>;<H1>,<H2>,...,<Hn>" "<integer>"
 #   where all ensembl IDs are separated by ","
 #   and all hugo symbols are separated by ","
 #   and the ensembl IDs are separated from hugo symbols by ";"
 # Note: ensembl IDs and hugo symbols are returned paired
 # i.e. if H2 is missing we have: "E1,E2,E3,..;H1,,H3,.."
-getGeneID <- function(chr, pos, map){
-g <- map[Reduce('intersect', list(which(paste("chr",map$chromosome_name, sep="") == chr),
-                                   which(map$start_position <= pos),
-                                   which(map$end_position >= pos))),]  
-  if( nrow(g) > 0){
+getGeneID <- function(pos, map){
+  if(is.null(map)){return("NA")}
+  
+  g <- map[intersect(which(map$start_position <= pos), 
+                   which(map$end_position >= pos)),]
+
+ if( nrow(g) > 0){
     g_e <- toString(g$ensembl_gene_id, sep=",")
     g_h <- toString(g$hgnc_symbol, sep=",")
-    return(paste(g_e, g_h, sep=";"))
-  } 
-  else {
+    g_ids <- paste(g_e, g_h, sep=";")
+    g_l <- toString(g$end_position - g$start_position, sep=",") 
+
+    return(cbind(g_ids, g_l))
+  }   else {
     return("NA")
-  }
+  } 
 }
 
 outputnames <- c("sampleID","cancerType","sequencingID","sequencingLane","sequencingBarcode","donorGeneID",
-                 "acceptorGeneID","donorChromosome","acceptorChromosome","donorEnd","acceptorEnd","name",
+                 "acceptorGeneID","donorGeneLength","acceptorGeneLength","donorChromosome","acceptorChromosome","donorEnd","acceptorEnd","name",
                  "coverage","strand","itemRgb","blockCount","blockSizes","blockStarts","entropy","flank_string_case",
                  "flank_string","min_mismatch","max_mismatch","average_mismatch","maximal_of_minimal_acceptor_site_length",
                  "maximal of minimal acceptor site length","minimal_anchor_difference","unique_read_count",
@@ -58,21 +57,40 @@ outputnames <- c("sampleID","cancerType","sequencingID","sequencingLane","sequen
                  "acceptor_sequence","match_to_gene_strand","fusion_source","fusion_type","gene_strand",
                  "donor_annotated_gene","acceptor_annotated_gene")
 
+clist <- as.list(c("BLCA","CESC","BRCA","HNSC","KIRP","KICH","KIRC","LGG","LIHC","LUAD","LUSC","PAAD","PRAD","SKCM","THCA"))
+ctnames <- unlist(clist)
+# input directory
+path <- "/Shares/work/DAT_109__TCGA_unc_rnaseq_genefusion/Data/mapped2TCGA_final/"
+
+d.clist <- lapply(clist, function(type){
+  name <- paste(path, type,".txt", sep="")
+  return(read.csv(name, header=T, sep="\t"))
+})
+
 for(ct in 1:length(d.clist)){
   df <- d.clist[[ct]]
+  
   # Get IDs of donor genes
-  genes_d <- apply(df, 1, function(x){
-                    return(getGeneID(x[7], x[8], map))
-                })
+  genes_d_l <- apply(df, 1, function(x){
+    chromosomenum <- substring(as.character(x['donorChromosome']), 4)
+    if(is.null(mapchr[[chromosomenum]])){return("NA")}
+    return(getGeneID(x['donorEnd'], mapchr[[chromosomenum]]))
+  })  
+  genes_d <- do.call("rbind", genes_d_l)
+  
   # Get IDs of acceptor genes
-  genes_a <- apply(df, 1, function(x){
-                    return(getGeneID(x[7], x[9], map))
-                })
-  delta <- length(outputnames) - ncol(df) - 2
-  padding <- data.frame(matrix(NA, nrow = nrow(output), ncol = delta))
-  output <- cbind(df[,1:5], genes_d, genes_a, df[,6:ncol(df)], padding)
+  genes_a_l <- apply(df, 1, function(x){
+    chromosomenum <- substring(x['acceptorChromosome'], 4)
+    if(is.null(mapchr[[chromosomenum]])){return("NA")}
+    return(getGeneID(x['acceptorEnd'], mapchr[[chromosomenum]]))
+  })
+  genes_a <- do.call("rbind", genes_a_l)
+  
+  delta <- length(outputnames) - ncol(df) - 4
+  padding <- data.frame(matrix(NA, nrow = nrow(df), ncol = delta))
+  output <- cbind(df[,1:5], genes_d[,'g_ids'], genes_a[,'g_ids'], genes_d[,'g_l'], genes_a[,'g_l'], df[,6:ncol(df)], padding)
   colnames(output) <- outputnames
-  filename=paste("/home/onikolov/projects/gene_fusion_unc/data/", names(d.clist)[ct], "_v2.tab", sep ="")
+  filename=paste("/home/onikolov/projects/gene_fusion_unc/data_gene_length/", ctnames[ct], ".tab", sep ="")
   write.table(output, file=filename, col.names=TRUE, row.names=FALSE, sep="\t")
   
 } # end for ct
